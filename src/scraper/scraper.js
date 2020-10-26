0fa7f014d3511b5638aa3511b360089322d0c422
zahidyew <zahidyew96@gmail.com>
2020-10-26 15:26:54 +0800

    work on scraper

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -1,25 +1,60 @@
 const request = require('superagent');
 const { parse } = require('node-html-parser');
 
-const scraper = async () => {
-   /* return request
-      .get('https://strangerthings.fandom.com/wiki/Dustin_Henderson')
-      .then(res => res.text)
-      .then(parse)
-      .then(titlesList)
-      .then(console.log); */
+async function scraper() {
+   const character = "Dustin_Henderson"
+   const url = 'https://strangerthings.fandom.com/wiki/' + character
 
-   const response = await request.get('https://strangerthings.fandom.com/wiki/Dustin_Henderson')
+   const response = await request.get(url)
    const data = await response.text
    const parseData = await parse(data)
-   const scrapedData = await born(parseData)
-   console.log(scrapedData)
+   
+   return parseData
 }
 
-const titlesList = html => html
-   .querySelectorAll('.pi-item.pi-item-spacing.pi-title')
-   .map(node => node.rawText);
+async function getName(html) {
+   return html.querySelectorAll('.pi-item.pi-item-spacing.pi-title').map(node => node.rawText);
+}
+   
+async function getCharacterSummary(html) {
+   return html.querySelectorAll('.pi-data-value.pi-font').map(node => node.rawText)
+}
 
-const born = html => html.querySelectorAll('.pi-data-value.pi-font').map(node => node.rawText)
+async function getPicture(html) {
+   return html.querySelectorAll('.pi-image-thumbnail')[0].getAttribute('src')
+}
 
-scraper();
+async function scrapeData() {
+   const parseData = await scraper()
+
+   const summary = await getCharacterSummary(parseData)
+
+   const name = await getName(parseData)
+   const picture = await getPicture(parseData)
+
+   const status = summary[0]
+   const born = summary[1]
+   const age = summary[2].substring(0,2)
+   const aliases = summary[3]
+   const relationshipStatus = summary[5]
+   const family = summary[6]
+   const otherRelations = summary[7]
+   const occupation = summary[9]
+   const gender = summary[10]
+   const playedBy = summary[13]
+
+   console.log("Name: " + name.toString())
+   console.log("Status: " + status)
+   console.log("Born: " + born)
+   console.log("Age: " + age)
+   console.log("Aliases: " + aliases)
+   console.log("Picture link: " + picture)
+   console.log("Relationship status: " + relationshipStatus)
+   console.log("Family: " + family)
+   console.log("Other Relations: " + otherRelations)
+   console.log("Occupation: " + occupation)
+   console.log("Gender: " + gender)
+   console.log("Played by: " + playedBy)
+}
+   
+scrapeData();
