936d28562aee38faec1e7dd354616d8e7a082b8c
zahidyew <zahidyew96@gmail.com>
2020-10-29 18:11:49 +0800

    remove useless script & do refactoring in scraper

--- src/scraper/getCharDetails.js
+++ src/scraper/getCharDetails.js
@@ -4,6 +4,71 @@ const { camelCase } = require('lodash')
 const fs = require("fs")
 const characterData = require("../../data/character")
 
+// main func. Make character obj and assign the right values to the correct label
+async function getDetails(character) {
+   const parseData = await scrapePage(character)
+   const summaryLabels = await getSummaryLabels(parseData)
+   const summaryValues = await getSummaryValues(parseData)
+   const name = await getName(parseData)
+   const picture = await getPicture(parseData)
+
+   let details = new Object()
+   details['name'] = name.toString()
+   details['picture'] = picture
+
+   if (summaryLabels.length === summaryValues.length) {
+      for (let i = 0; i < summaryLabels.length; i++) {
+         const label = camelCase(summaryLabels[i])
+         const structure = summaryValues[i].innerHTML
+         let value = summaryValues[i].rawText
+
+         if (label == 'status') {
+            details[label] = removeBrackets(value, '[')
+         }
+         else if (label == 'born') {
+            details[label] = removeBrackets(value, '[')
+         }
+         else if (label == 'age') {
+            // need to remove 2 types of brackets
+            value = removeBrackets(value, '[')
+            details[label] = removeBrackets(value, '(')
+         }
+         else if (label == 'aliases') {
+            details[label] = splitData(structure)
+         }
+         else if (label == 'relationshipStatus') {
+            details[label] = value
+         }
+         else if (label == 'family') {
+            details[label] = splitData(structure)
+         }
+         else if (label == 'otherRelations') {
+            details[label] = splitData(structure)
+         }
+         else if (label == 'affiliation') {
+            details[label] = splitData(structure)
+         }
+         else if (label == 'occupation') {
+            details[label] = splitData(structure)
+         }
+         else if (label == 'gender') {
+            details[label] = value
+         }
+         else if (label == 'portrayedBy') {
+            details[label] = splitData(structure)
+         }
+         else {
+            //console.log(label + " null")
+         }
+      }
+      details = checkProperty(details)
+      console.log(details)     // uncomment this when testing
+      //writeDataToFile(details)   // make sure character.json file is empty, with only []. Comment when still testing
+   } else {
+      console.log("Error at getDetails func, labels array not the same size as values array.")
+   }
+}
+
 // scrape each character page
 async function scrapePage(character) {
    const url = 'https://strangerthings.fandom.com/wiki/' + character
@@ -33,71 +98,6 @@ async function getSummaryValues(html) {
 // get the link for character's picture 
 async function getPicture(html) {
    return html.querySelectorAll('.pi-image-thumbnail')[0].getAttribute('src')
-}
-
-// make character obj and assign the right values to the correct label
-async function getDetails(character) {
-   const parseData = await scrapePage(character)
-   const summaryLabels = await getSummaryLabels(parseData)
-   const summaryValues = await getSummaryValues(parseData)
-   const name = await getName(parseData)
-   const picture = await getPicture(parseData)
-
-   let details = new Object()
-   details['name'] = name.toString()
-   details['picture'] = picture
-
-   if (summaryLabels.length === summaryValues.length) {      
-      for (let i = 0; i < summaryLabels.length; i++) {
-         const label = camelCase(summaryLabels[i])
-         const structure = summaryValues[i].innerHTML
-         let value = summaryValues[i].rawText
-
-         if (label == 'status') {
-            details[label] = removeBrackets(value, '[')
-         } 
-         else if (label == 'born') {
-            details[label] = removeBrackets(value, '[')
-         } 
-         else if (label == 'age') {
-            // need to remove 2 types of brackets
-            value = removeBrackets(value, '[')
-            details[label] = removeBrackets(value, '(')
-         } 
-         else if (label == 'aliases') { 
-            details[label] = splitData(structure)
-         } 
-         else if (label == 'relationshipStatus') {
-            details[label] = value
-         } 
-         else if (label == 'family') {
-            details[label] = splitData(structure)
-         } 
-         else if (label == 'otherRelations') {
-            details[label] = splitData(structure)
-         } 
-         else if (label == 'affiliation') {
-            details[label] = splitData(structure)
-         } 
-         else if (label == 'occupation') {
-            details[label] = splitData(structure)
-         } 
-         else if (label == 'gender') {
-            details[label] = value
-         } 
-         else if (label == 'portrayedBy') {
-            details[label] = splitData(structure)
-         } 
-         else {
-            //console.log(label + " null")
-         }
-      } 
-      details = checkProperty(details)
-      //console.log(details)     // uncomment this when testing
-      writeDataToFile(details)   // make sure character.json file is empty, with only []. Comment when still testing
-   } else {
-      console.log("Error at getDetails func, labels array not the same size as values array.")
-   }  
 }
 
 // remove '[]' or '()' from the data
@@ -134,6 +134,26 @@ function splitData(string) {
    }
 }
 
+// remove 'a href' links & rare '<sup' elements
+function removeHref(str) {
+   str = removeTags(str)
+   // find all occurence(s) of '<a' in the string
+   const found = str.match(/<a/g)
+
+   // if exists, remove all the '<a' elements from the string
+   if (found !== null) {
+      for (let i = 0; i < found.length; i++) {
+         str = erase(str, '<a', '>')
+      }
+   }
+
+   if (str.includes('<sup')) {
+      str = erase(str, '<sup', '</sup>')
+   }
+
+   return str
+}
+
 // remove html tags
 function removeTags(str) {
    // If separator is a regex that contains capturing parentheses (), matched results are included in the array.
@@ -144,31 +164,13 @@ function removeTags(str) {
    return str
 }
 
-// remove 'a href' links & rare '<sup' elements
-function removeHref(str) {
-   str = removeTags(str)
-   // find all occurence(s) of '<a' in the string
-   const found = str.match(/<a/g)
+// erase unwanted portion of a string by replacing it with ""
+function erase(string, startMarker, endMarker) {
+   let start = string.indexOf(startMarker)
+   let end = string.indexOf(endMarker)
+   let toBeErased = string.substring(start, end + endMarker.length)
 
-   // if exists, remove all the '<a' elements from the string
-   if (found !== null) {
-      for (let i = 0; i < found.length; i++) {
-         let start = str.indexOf('<a')
-         let end = str.indexOf('>')
-         let s = str.substring(start, end + 1)
-
-         str = str.replace(s, "")
-      }
-   } 
-   if (str.includes('<sup')) {
-      let start = str.indexOf('<sup')
-      let end = str.indexOf('</sup>')
-      let s = str.substring(start, end + 6)
-
-      str = str.replace(s, "")
-   }
-
-   return str
+   return string = string.replace(toBeErased, "")
 }
 
 // some characters has unmentioned details, so we need to check and assign accordingly 
